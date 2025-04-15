cc6aa03bab28163937122e5b1fd35e4c4dce5848
anudev <eight.atulya@gmail.com>
2025-04-15 07:16:32 +0530

    e*

--- docker/run/fs/ins/install_playwright.sh
+++ docker/run/fs/ins/install_playwright.sh
@@ -7,13 +7,11 @@
 pip install playwright
 
 # install chromium with dependencies
-playwright install chromium
-
-# Ensure the Playwright cache directory exists and contains Chromium
-CACHE_DIR="$HOME/.cache/ms-playwright/chromium"
-if [ ! -e "$CACHE_DIR/chrome-linux/headless_shell" ]; then
-    echo "Chromium binary not found. Reinstalling..."
-    playwright install chromium
+# for kali-based
+if [ "$@" = "hacking" ]; then
+    apt-get install -y fonts-unifont libnss3 libnspr4
+    playwright install chromium-headless-shell
 else
-    echo "Chromium binary is already installed."
+    # for debian based
+    playwright install --with-deps chromium-headless-shell
 fi
